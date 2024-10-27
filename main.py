f2ecdd93c432599fed0f014ba4eebcf41ed78e26
IGoWacky <sekunoshikanlu@gmail.com>
2024-10-26 20:51:02 -0400

    Imported necessary libraries and loaded the dataset

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
 from sklearn.datasets import fetch_california_housing
 import pandas as pd
+
+data = fetch_california_housing()
+dataframe = pd.DataFrame(data.data, columns=data.feature_names)
+dataframe['price'] = data.target
