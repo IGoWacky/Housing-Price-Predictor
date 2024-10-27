343c80ed1b9e314bb9e50494e61b61eac16452ec
IGoWacky <sekunoshikanlu@gmail.com>
2024-10-26 23:08:40 -0400

    syntax

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from sklearn.datasets import fetch_california_housing
-from sklearn.model_selection import train_test_split
+from sklearn.datasets import fetch_california_housing as housing
+from sklearn.model_selection import train_test_split as testSplit
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error, root_mean_squared_error
 import pandas as pd
 
-data = fetch_california_housing()
+data = housing()
 dataframe = pd.DataFrame(data.data, columns=data.feature_names)
 dataframe['price'] = data.target
 
@@ -12,7 +12,7 @@ dataframe = dataframe.drop(columns=['Population'])
 
 x = dataframe.drop(columns=['price']) #features to be analyzed
 y = dataframe['price'] #target variable
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=50)
+x_train, x_test, y_train, y_test = testSplit(x, y, test_size=0.2, random_state=50)
 
 model = RandomForestRegressor(n_estimators=100, max_depth=100, random_state=50)
 model.fit(x_train, y_train) #this trains the model and the previous line initializes it
