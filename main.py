98ec7434f086c06c054d45c0f026a45cc143a438
IGoWacky <sekunoshikanlu@gmail.com>
2024-10-27 14:50:21 -0400

    finished website

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from sklearn.datasets import fetch_california_housing as housing
 from sklearn.model_selection import train_test_split as testSplit
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error, root_mean_squared_error
+from flask import Flask, request, render_template, jsonify
 import pandas as pd
 
 data = housing()
@@ -16,7 +17,35 @@ x_train, x_test, y_train, y_test = testSplit(x, y, test_size=0.2, random_state=5
 
 model = RandomForestRegressor(n_estimators=100, max_depth=100, random_state=50)
 model.fit(x_train, y_train) #this trains the model and the previous line initializes it
-y_prediction = model.predict(x_test)
 
-mAbsErr = mean_absolute_error(y_test, y_prediction)
-rmSqErr = root_mean_squared_error(y_test, y_prediction)
+app = Flask(__name__)
+@app.route('/')
+def form():
+    return render_template("index.html")
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    med_inc = float(request.form['med_inc'])
+    house_age = float(request.form['house_age'])
+    ave_rooms = float(request.form['ave_rooms'])
+    ave_bedrooms = float(request.form['ave_bedrooms'])
+    ave_occupancy = float(request.form['ave_occupancy'])
+    latitude = float(request.form['latitude'])
+    longitude = float(request.form['longitude'])
+
+    user_data = pd.DataFrame({
+        'MedInc': [med_inc],
+        'HouseAge': [house_age],
+        'AveRooms': [ave_rooms],
+        'AveBedrms': [ave_bedrooms],
+        'AveOccup': [ave_occupancy],
+        'Latitude': [latitude],
+        'Longitude': [longitude]
+    })
+
+    prediction = model.predict(user_data)
+    predicted_price = round(prediction[0], 2)
+    return render_template('index.html', prediction_text=f"Predicted House Price: ${round(predicted_price, 2)}")
+
+if __name__ == "__main__":
+    app.run(debug=True)
